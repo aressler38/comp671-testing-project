94225b26c4e0082561ee50b85bbf4963a89037f6
siva01224 <kudumula.sivareddy1991@gmail.com>
2018-10-27 13:08:54 -0700

    fix the subtraction test

--- features/step_definitions/subtraction.stepdefs.js
+++ features/step_definitions/subtraction.stepdefs.js
@@ -6,21 +6,21 @@ const {bc} = require('../../src/bc.js');
 
 
 
-Given('a is 100', function () {
+Given('For subtraction, a is 100', function () {
     this.a = 100;
 });
 
-Given('b is {int}', function (int) {
+Given('For subtraction, b is {int}', function (int) {
     this.b = int;
 });
 
-When('a is subtracted to b', function () {
+When('b is subtracted from a', function () {
     // Create our expression to pass to the 'bc' calculator command.
     const expression = `${this.a} - ${this.b}`;
     // Run the calculator.
     this.result = bc(expression);
 });
 
-Then('I should get {int}', function (int) {
+Then('For subtraction, I should get {int}', function (int) {
     assert.strictEqual(this.result, int);
 });
--- features/substration.feature
+++ features/substration.feature
@@ -1,8 +1,8 @@
-Feature: Subtraction is handled properly
+Feature:  Subtraction is handled properly
     A calculator should be able to minus numbers together and return the correct result.
 
-    Scenario: Additive Identity
+    Scenario: Subtraction 
         Given a is 100
         Given b is 99
-        When a is subtracted to b
+        When b is subtracted from a
         Then I should get 1
